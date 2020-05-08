0f5a4845f68c09d37701f04587644c3fd05ae8b0
Jaimy Tieck <127511@student.drenthecollege.nl>
2020-05-08 13:49:11 +0200

    Test of het werkt met github

--- index.php
+++ index.php
@@ -3,10 +3,10 @@
   <head>
     <meta charset="utf-8">
     <title></title>
-    <link rel="stylesheet" href="index.css">
   </head>
   <body>
     <h1>abcde</h1>
     <h2>efghi</h2>
+    <h3>UREKA! HET WERKT!</h3>
   </body>
 </html>
